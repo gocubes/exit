f57b5c3b168fd3879663f669d8741977ba8129c4
Frank Kung <kfanjian@gmail.com>
2015-12-03 22:46:00 +0800

    add comments to example.

--- example/main.go
+++ example/main.go
@@ -9,12 +9,15 @@ import (
 
 func main() {
 	fmt.Println("Start some service.")
+	// Wait interrupt signal, and set things number(0-255) should todo before exit.
 	exit.Wait(0)
 
 	for {
 		time.Sleep(1e6)
+		// Received an interrupt signal
 		if exit.Exit == true {
 			fmt.Println("Clean some things")
+			// Done.
 			exit.Todo.Done()
 			break
 		}
